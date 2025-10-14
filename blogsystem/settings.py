dee83cd66ed6dc3bbc1e183003135d8bf07db12b
GordenArcher <archergorden@gmail.com>
2025-10-14 21:46:00 +0000

    rename auth app to authentication to avoid conflicts

--- blogsystem/settings.py
+++ blogsystem/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'auth'
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
